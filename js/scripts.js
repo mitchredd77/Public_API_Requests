1cde2e62bca122c703259863d48c34995edd23fd
mitchredd77 <147752683+mitchredd77@users.noreply.github.com>
2024-02-19 21:29:22 -0700

    Working on Modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-const url = "https://randomuser.me/api/" 
 const gallery = document.getElementById("gallery");
 
 // Pull 12 random employees from the api
@@ -32,6 +31,35 @@ function generateHTML(employee) {
     const cardDiv = document.createElement('div');
     cardDiv.innerHTML = employeeHTML;
     gallery.insertAdjacentElement('beforeend', cardDiv);
+    
+    cardDiv.addEventListener('click', (e) => {
+      e.preventDefault();
+      modalHTML(employee);
+  });
   }
+function modalHTML(employee) {
+  const body = document.querySelector('body');
+  const modalHTML = `
+  <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+                        <p class="modal-text">email</p>
+                        <p class="modal-text cap">city</p>
+                        <hr>
+                        <p class="modal-text">(555) 555-5555</p>
+                        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                        <p class="modal-text">Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+                `;
+    const modalDiv = document.createElement('div');
+    modalDiv.innerHTML = modalHTML;
+    body.insertAdjacentHTML('beforeend', modalDiv);
+  }
+fetchEmployees();
 
-fetchEmployees()
+
+//  Don't use body to select for the modal. Select within the function..
